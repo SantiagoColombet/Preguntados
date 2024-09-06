b60e310baa04659b725f5ee4c5a30df7c93d8ced
EITÁN GABRIEL FRIDMAN <48242472@ort>
2024-09-06 16:01:11 -0300

    commit 12

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -10,3 +10,18 @@ closeButton.addEventListener('click', () => {
     configMenu.style.display = 'none';
 });
 
+
+
+// document.querySelector('.ruleta').addEventListener('click', function() {
+//     const grados = Math.floor(Math.random() * 360);
+//     this.style.transform = `rotate(${grados}deg)`;
+// });
+
+let container = document.querySelector(".container");
+let btn = document.getElementById("spin");
+let number = Math.ceil(Math.random() * 1000);
+
+btn.onclick = function () {
+	container.style.transform = "rotate(" + number + "deg)";
+	number += Math.ceil(Math.random() * 1000);
+}
